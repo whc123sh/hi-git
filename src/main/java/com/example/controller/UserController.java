dacdef14dfa24e68a69c0f86ac0332d618fc7132
whc <13343456793@163.com>
2021-02-05 16:58:04 +0800

    暂时去掉日志配置

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.example.controller;
 
-import com.example.domain.User;
-import com.example.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @RestController
 public class UserController {
-    @Autowired
-    private UserService userService;
 
     @RequestMapping("/test")
     public String login() {
-        User user = userService.selectUserById();
-        return user.toString();
+        return "hi";
     }
 
 }
